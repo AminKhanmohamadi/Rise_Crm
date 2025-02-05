af2af64cc11cde08fcd6fe9623a22bfec6908b0d
amin <aminkhm097@gmail.com>
2025-02-05 19:10:16 +0330

    fix: admin panel

--- backend/src/projects/admin.py
+++ backend/src/projects/admin.py
@@ -4,5 +4,14 @@ from projects.models import Tags , Task , Project
 
 @admin.register(Tags)
 class TagsAdmin(admin.ModelAdmin):
-    fields=['name']
+    list_display=['name']
     
+ 
+@admin.register(Project)
+class ProjectsAdmin(admin.ModelAdmin):
+    list_display=['title' , 'customer__name']   
+    
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title' , 'project']
